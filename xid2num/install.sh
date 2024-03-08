8d5f962311515210e758fb59502ddbed1100903b
winter.loo <adjkldd@126.com>
2024-03-08 10:02:18 +0800

    remove xid2num

--- xid2num/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-
-make install
-
-psql -c "drop extension if exists xid2num;create extension xid2num;"
--- xid2num/xid2num--1.0.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE FUNCTION xid2num()
-    RETURNS text
-    AS 'xid2num', 'xid2num'
-    LANGUAGE C
-    STRICT;
